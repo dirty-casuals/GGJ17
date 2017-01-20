a883b44ddfe75347d819ff8f42b60a1197aa853a
ReverendOfRustles <tpapgjmq@sharklasers.com>
2017-01-20 23:33:08 +0000

    Some basic layout for player interaction zones

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    public enum InputType
+    {
+        PIT_UP,
+        PIT_DOWN,
+        PIT_LEFT,
+        PIT_RIGHT,
+
+        PIT_INTERACT,
+        PIT_ATTACK
+    };
+
+    public const string HumanTag = "Pawn";
+
+    public const int PlayerSpeedX = 6;
+    public const int PlayerSpeedZ = 6;
+
+    public const KeyCode upKey = KeyCode.W;
+    public const KeyCode downKey = KeyCode.S;
+    public const KeyCode leftKey = KeyCode.A;
+    public const KeyCode rightKey = KeyCode.D;
+
+    public const KeyCode interactionKey = KeyCode.E;
+    public const KeyCode attackKey = KeyCode.Space;
+}
---
+++ Assets/Scripts/Interaction/InteractionPoint.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionPoint : MonoBehaviour
+{
+    public enum InteractionPointType
+    {
+        IPT_INVALID,
+
+        IPT_CASHIER_TILL,
+        IPT_FOOD_PRODUCT,
+        IPT_FREEZER
+    };
+
+    public InteractionPointType eInteractionType;
+    public float fInteractionRadius = 0.5f;
+
+    private SphereCollider sCollision;
+
+	// Use this for initialization
+	void Start () {
+        sCollision = gameObject.AddComponent<SphereCollider>();
+
+        sCollision.radius = fInteractionRadius;
+        sCollision.isTrigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == Constants.HumanTag)
+        {
+            if(PlayerInput.QueryPlayerInput(Constants.InputType.PIT_INTERACT, true))
+            {
+                ProcessInteraction(other.gameObject);
+            }
+        }
+    }
+
+    private void ProcessInteraction(GameObject other)
+    {
+        Debug.Log("Player Interacted with me");
+        other.SendMessage("HandleInteraction", this.gameObject);
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        switch(eInteractionType)
+        {
+            case InteractionPointType.IPT_INVALID:
+                Gizmos.color = Color.red;
+            break;
+
+            case InteractionPointType.IPT_CASHIER_TILL:
+                Gizmos.color = new Color(255, 128, 0);
+            break;
+
+            case InteractionPointType.IPT_FOOD_PRODUCT:
+                Gizmos.color = Color.green;
+            break;
+
+            case InteractionPointType.IPT_FREEZER:
+                Gizmos.color = Color.blue;
+            break;
+        }
+        
+        Gizmos.DrawWireSphere(transform.position, fInteractionRadius);
+    }
+}
