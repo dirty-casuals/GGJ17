c9167cb47952409985dbb3a7c88f6895063c7334
Izzo <hristoz2004@gmail.com>
2017-01-20 23:30:58 +0000

    Add items and customerAI looking for them.

---
+++ Assets/CustomerAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CustomerAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    ShoppingItem[] targetItems;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        SetRandomItemTargets();
+        agent.SetDestination( targetItems[0].position );
+    }
+
+    private void SetRandomItemTargets()
+    {
+        HashSet<ShoppingItem> itemIds = new HashSet<ShoppingItem>();
+
+        int numTargets = Random.Range(2,3);
+        while( itemIds.Count <= numTargets )
+        {
+            int id = Random.Range(0, ShoppingItem.items.Length-1 );
+            itemIds.Add( ShoppingItem.items[id] );
+        }
+
+        targetItems = new ShoppingItem[itemIds.Count];
+        itemIds.CopyTo( targetItems );
+    }
+
+    bool SeesItem( ShoppingItem item )
+    {
+        return Vector3.Distance( transform.position, item.position ) < 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/ShoppingItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingItem : MonoBehaviour
+{
+    public static ShoppingItem[] items;
+
+    public Vector3 originalPlace;
+
+    public string itemName { get { return gameObject.name; } }
+
+    public Vector3 position { get { return transform.position; } }
+
+    void Start()
+    {
+        originalPlace = transform.position;
+
+        if( items == null || items.Length == 0 || items[0] == null )
+        {
+            items = FindObjectsOfType<ShoppingItem>();
+        }
+    }
+}
