dd621f88e86f2e5b74442790c619e39a43e57d29
Izzo <hristoz2004@gmail.com>
2017-01-21 00:23:25 +0000

    Add a quick and dirty multi-target AI implementation.
    They now go to multiple item locations, then the cashier and then they leave.

--- Assets/CustomerAI.cs
+++ Assets/CustomerAI.cs
@@ -1,20 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CustomerAI : MonoBehaviour
 {
+    private const int SIGHT_RADIUS = 1;
+    private const int DISTANCE_FROM_DESTINATION = 1;
     NavMeshAgent agent;
 
     ShoppingItem[] targetItems;
+
+    private int currentItemIdx = -1;
 
     // Use this for initialization
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         SetRandomItemTargets();
-        agent.SetDestination( targetItems[0].position );
+        GotoNextTarget();
+
+    }
+
+    private void GotoNextTarget()
+    {
+        currentItemIdx++;
+        if( currentItemIdx < targetItems.Length )
+        {
+            agent.SetDestination( targetItems[currentItemIdx].position );
+        }
+        else if( currentItemIdx == targetItems.Length )
+        {
+            GameObject till = GameObject.FindGameObjectWithTag( "Checkout" );
+            agent.SetDestination( till.transform.position );
+        }
+        else
+        {
+            GameObject exit = GameObject.FindGameObjectWithTag( "Gate" );
+            agent.SetDestination( exit.transform.position );
+        }
     }
 
     private void SetRandomItemTargets()
@@ -32,13 +55,35 @@ public class CustomerAI : MonoBehaviour
         itemIds.CopyTo( targetItems );
     }
 
+    bool SeesItem()
+    {        
+        ShoppingItem targetItem = targetItems[currentItemIdx];
+        return SeesItem( targetItem );
+    }
+
     bool SeesItem( ShoppingItem item )
     {
-        return Vector3.Distance( transform.position, item.position ) < 1;
+        return Vector3.Distance( transform.position, item.position ) < SIGHT_RADIUS;
+    }
+
+    private bool LookingForItem()
+    {
+        return currentItemIdx < targetItems.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if( agent.remainingDistance <= DISTANCE_FROM_DESTINATION )
+        {
+            if( LookingForItem() && SeesItem() )
+            {
+                GotoNextTarget();
+            }
+            else if( !LookingForItem() )
+            {
+                GotoNextTarget();
+            }
+        }
     }
 }
