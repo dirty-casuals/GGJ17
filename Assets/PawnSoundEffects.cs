c4055cbe17d89fb7d71f9c49764e81a9d2f65872
Izzo <hristoz2004@gmail.com>
2017-01-22 17:06:46 +0000

    Add sound effect to the player.

---
+++ Assets/PawnSoundEffects.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnSoundEffects : MonoBehaviour
+{
+    public AudioClip pickupSound;
+    public AudioClip leaveSound;
+    public AudioClip hitSound;
+    public AudioClip tillSwipe;
+
+    AudioSource audioSource;
+    IPawn pawn;
+
+    void Start()
+    {
+        pawn = GetComponentInParent<IPawn>();
+        audioSource = GetComponent<AudioSource>();
+        pawn.onPickupItem += OnPickupItem;
+        pawn.onLeaveItem += OnLeaveItem;
+        pawn.onHit += OnHit;
+    }
+
+    private void OnHit()
+    {
+        PlaySFX( hitSound );
+    }
+
+    private void OnLeaveItem()
+    {
+        PlaySFX( leaveSound );
+    }
+
+    private void OnPickupItem()
+    {
+        PlaySFX( pickupSound );
+    }
+
+    public void OnSwipeSoundEffectPlay()
+    {
+        PlaySFX( tillSwipe );
+    }
+
+    private void PlaySFX( AudioClip clip )
+    {
+        if( !audioSource.isPlaying )
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+}
